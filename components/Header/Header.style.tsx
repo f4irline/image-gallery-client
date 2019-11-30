2c3457e7ba694a21e835dad793894bf53deee11c
Tommi Lepola <lepola.tommi@gmail.com>
2019-11-30 22:22:26 +0200

    Create custom header

---
+++ components/Header/Header.style.tsx
@@ -0,0 +1,40 @@
+import { StyleSheet, ViewStyle, StatusBar, TextStyle, Dimensions } from 'react-native';
+
+interface Styles {
+    headerContainer: ViewStyle;
+    actionContainer: ViewStyle;
+    titleContainer: ViewStyle;
+    title: TextStyle;
+    subtitle: TextStyle;
+}
+
+const styles: StyleSheet.NamedStyles<Styles> = StyleSheet.create<Styles>({
+    headerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: 80,
+        width: Dimensions.get('window').width,
+        paddingTop: StatusBar.currentHeight,
+        backgroundColor: '#222831'
+    },
+    actionContainer: {
+        padding: 10,
+    },
+    titleContainer: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        padding: 5,
+    },
+    title: {
+        color: '#eeeeee',
+        fontSize: 18,
+    },
+    subtitle: {
+        color: '#c7c7c7',
+        fontSize: 12,
+    }
+})
+
+export default styles;
---
+++ components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Platform, View, Text } from 'react-native';
+import { MaterialIcons as MaterialIcon, Ionicons as IonIcon } from '@expo/vector-icons';
+
+import { HeaderProps } from 'react-navigation-stack';
+import headerStyles from './Header.style';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+interface Props {
+    headerProps: HeaderProps;
+    title: string;
+    subtitle?: string;
+}
+
+const Header: React.FC<Props> = (props: Props) => {
+    const { headerProps, title, subtitle } = props;
+    const isIos = Platform.OS === 'ios';
+    return (
+        <View style={headerStyles.headerContainer}>
+            <TouchableOpacity style={headerStyles.actionContainer} onPress={() => headerProps.navigation.pop()}>
+                { isIos ? <IonIcon name={'ios-arrow-back'} size={25} color={'#eeeeee'}/> : <MaterialIcon size={25} name={'arrow-back'} color={'#eeeeee'}/>  }
+            </TouchableOpacity>
+            <View style={headerStyles.titleContainer}>
+                <Text style={headerStyles.title}>{title}</Text>
+                { subtitle ? <Text style={headerStyles.subtitle}>{subtitle}</Text> : undefined }
+            </View>
+        </View>
+    )
+}
+
+export default Header;
