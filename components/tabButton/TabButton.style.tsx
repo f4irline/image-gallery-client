bf0219c63023a4a0323fe679d5ef085229e6ef04
Tommi Lepola <lepola.tommi@gmail.com>
2019-12-04 18:20:19 +0200

    Implement tabs for profile page to select different kind of content

---
+++ components/tabButton/TabButton.style.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, ViewStyle, TextStyle } from 'react-native';
+
+interface Styles {
+    buttonContainer: ViewStyle;
+    active: ViewStyle;
+    buttonText: TextStyle;
+}
+
+const styles: StyleSheet.NamedStyles<Styles> = StyleSheet.create<Styles>({
+    buttonContainer: {
+        flex: 1,
+        borderWidth: 2,
+        borderColor: '#c7c7c7',
+        backgroundColor: '#353d4a',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        color: '#eeeeee',
+        textTransform: 'uppercase',
+        fontFamily: 'Rubik-Bold',
+    },
+    active: {
+        borderColor: '#ff5722'
+    }
+})
+
+export default styles;
---
+++ components/tabButton/TabButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import styles from './TabButton.style';
+
+interface Props {
+    label: string;
+    onClick: () => void;
+    isSelected?: boolean;
+}
+
+const TabButton = (props: Props) => {
+    return (
+        <TouchableOpacity style={[styles.buttonContainer, props.isSelected ? styles.active : undefined]} onPress={props.onClick}>
+            <Text style={styles.buttonText}>{props.label}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export default TabButton;
