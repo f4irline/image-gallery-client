12a69ea35451411bf800cda378c6a55d406470d0
Tommi Lepola <lepola.tommi@gmail.com>
2019-12-08 11:00:28 +0100

    Add validator utility file. Fix Api utility file format.

---
+++ utils/validator/user.validator.ts
@@ -0,0 +1,4 @@
+
+export const validateUsername = (name: string): boolean => !!name.match('/^\w{5,}$/g');
+
+export const validatePassword = (password: string): boolean => !!password.match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/g')
